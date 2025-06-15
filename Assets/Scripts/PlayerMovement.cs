c2bb29db68b080256d98b880e992f33148b7a384
MiguelPereira45 <miguelpereira222005@gmail.com>
2025-06-15 16:42:25 +0100

    Nivel 1, Nivel 2. Nivel 3 jogo do labirinto

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,50 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerCtrl :MonoBehaviour
+public class PlayerCtrl : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
     private Rigidbody2D rb;
     private float speedX;
     private float speedY;
+    private Vector3 posicaoInicial;
 
-    void Start ()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        posicaoInicial = transform.position;
     }
 
-    void Update ()
+    void Update()
     {
         speedX = Input.GetAxisRaw("Horizontal");
         speedY = Input.GetAxisRaw("Vertical");
     }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(speedX * moveSpeed, speedY * moveSpeed);
+        rb.velocity = new Vector2(speedX * moveSpeed, speedY * moveSpeed);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Colisão com: " + collision.gameObject.tag);
+
+        if (collision.gameObject.CompareTag("box"))
+        {
+            Debug.Log("TOCOU NA BOX");
+            transform.position = posicaoInicial;
+        }
+        else if (collision.gameObject.CompareTag("frango"))
+        {
+            Debug.Log("TOCOU NO FRANGO");
+            SceneManager.LoadScene("Derrota");
+        }
+        else if (collision.gameObject.CompareTag("melancia"))
+        {
+            Debug.Log("TOCOU NA MELANCIA");
+            
+        }
     }
 }
